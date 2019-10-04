d2e0b882125591e5ea2ba8c58d9cce46eddb9dcc
Adland Lee <adland@4mfd.com>
2019-10-04 14:14:31 -0700

    bullseye HSL

---
+++ 2 Bullseye HSL/Bullseye HSL/Bullseye HSL/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Bullseye HSL
+//
+//  Created by Adland Lee on 10/4/19.
+//  Copyright © 2019 Adland Lee. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
